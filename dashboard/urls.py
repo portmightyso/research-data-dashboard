cf3b8b10b6d2a606deaf98993acdcba326d65ab7
Portmightyso <sorooshina.golmohammadi@gmail.com>
2026-05-19 12:06:51 +0200

    Auto-create superuser on deployment

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path
-from .views import ProjectListAPI, DashboardHome, LoginView, logout_view
+from .views import (
+    DashboardHome, 
+    LoginView, 
+    logout_view, 
+    ProjectAPI, 
+    ExperimentLogAPI
+)
 
 urlpatterns = [
+    # صفحه اصلی داشبورد
     path('', DashboardHome.as_view(), name='dashboard_home'),
+    
+    # احراز هویت و مدیریت جلسات کاربری
     path('login/', LoginView.as_view(), name='login_view'),
     path('logout/', logout_view, name='logout_view'),
-    path('api/projects/', ProjectListAPI.as_view(), name='project_api'),
+    
+    # APIهای فرانت‌آند (Vue.js) برای ارتباط با دیتابیس
+    path('api/projects/', ProjectAPI.as_view(), name='project_api'),
+    path('api/experiments/', ExperimentLogAPI.as_view(), name='experiment_api'),
 ]
